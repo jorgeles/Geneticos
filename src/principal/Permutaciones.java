8de8bf59139dab002337239b45d948bb2d4a1bb4
jorgeles <jorgeleonfernandez@gmail.com>
2014-12-16 13:07:20 +0100

    commit

--- src/principal/Permutaciones.java
+++ src/principal/Permutaciones.java
@@ -6,10 +6,10 @@ public class Permutaciones {
 
 	public static void main(String[] args) {
 		// String[] elementos = "a,b,c,d,e".split(",");
-
+		ArrayList<ArrayList<Integer>> soluciones = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> solucion = new ArrayList<Integer>();
 		int size = 150;
-		int max = 4;
+		int max = 6;
 		int min = 0;
 		int creadas = 0;
 		while (max <= size) {
@@ -21,32 +21,33 @@ public class Permutaciones {
 			}
 			int n = elementos.size(); // Tipos para escoger
 			int r = elementos.size(); // Elementos elegidos
-			Perm2(elementos, solucion, n, r);
+			Perm2(elementos, solucion, n, r,soluciones);
 			if (max < size) {
 				min=max;
-				max = max + 4;
+				max = max + 6;
 				if (max > size) {
 					max = size;
 				}
 			} else if (max == size) {
 				max = max + 1;
 			}
-
+			System.out.println(soluciones.size());
 		}
-
+		System.out.println(soluciones.get((720*2)-1)+" "+creadas);
 	}
 
 	private static void Perm2(ArrayList<Integer> elem, ArrayList<Integer> act,
-			int n, int r) {
+			int n, int r, ArrayList<ArrayList<Integer>> soluciones) {
 		if (n == 0) {
-			System.out.println(act);
+			//System.out.println(act);
+			soluciones.add(act);
 		} else {
 			for (int i = 0; i < r; i++) {
 				if (!act.contains(elem.get(i))) { // Controla que no haya
 													// repeticiones
 					ArrayList<Integer> act2 = (ArrayList<Integer>) act.clone();
 					act2.add(elem.get(i));
-					Perm2(elem, act2, n - 1, r);
+					Perm2(elem, act2, n - 1, r, soluciones);
 				}
 			}
 		}
